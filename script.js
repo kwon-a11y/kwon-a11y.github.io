206c21f94bbf99358a09a6faaab458e822a5a6ec
kwon-a11y <kwon-a11y@example.com>
2025-02-07 11:30:29 +0900

    Update Discord OAuth2 URL in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('login-button').addEventListener('click', () => {
     // Discord OAuth2 URL을 여기에 추가합니다.
-    const discordOAuthURL = 'https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code&scope=identify%20guilds.join';
+    const discordOAuthURL = 'https://discord.com/api/oauth2/authorize?client_id=1337231980009492640&redirect_uri=https://kwon-a11y.github.io/callback&response_type=code&scope=identify%20guilds.join';
     window.location.href = discordOAuthURL;
 });
